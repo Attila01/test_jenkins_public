5342909b3c77ce83b19b8ff626b6f3e75403bdbc
Attila01 <48849467+Attila01@users.noreply.github.com>
2023-07-20 18:30:21 -0400

    Create addition_script.sh

---
+++ addition_script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Function to perform addition
+function addition() {
+  result=$(($1 + $2))
+}
+
+# Main script starts here
+
+echo "Please enter two numbers separated by a space:"
+read num1 num2
+
+addition $num1 $num2
+echo "The sum of $num1 and $num2 is: $result"
