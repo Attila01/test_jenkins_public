fd9582742c7d89716511bfde6091f76e8c048aa7
Attila01 <48849467+Attila01@users.noreply.github.com>
2023-07-20 18:59:42 -0400

    Create arithmetic.sh

---
+++ arithmetic.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# this script shows arithmetic operations
+echo “Addition of $num1 & $num2 is $[$num1 + $num2]”
+echo “Subtraction of $num1 & $num2 is $[$num1 - $num2]
+var3=$[$num1 / $num2] echo $var3
