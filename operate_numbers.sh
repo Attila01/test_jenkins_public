05be7c0052bd324901bc6628da0f77ac132e4ccb
Attila01 <48849467+Attila01@users.noreply.github.com>
2023-07-20 18:40:49 -0400

    Update operate_numbers.sh

--- operate_numbers.sh
+++ operate_numbers.sh
@@ -2,53 +2,17 @@
 
 # Function to perform addition
 function addition() {
-  result=$(($1 + $2))
+  local sum=0
+  for num in "$@"; do
+    ((sum += num))
+  done
+  result=$sum
 }
 
-# Function to perform subtraction
-function subtraction() {
-  result=$(($1 - $2))
-}
-
-# Function to perform multiplication
-function multiplication() {
-  result=$(($1 * $2))
-}
-
-# Function to perform division
-function division() {
-  if [ $2 -ne 0 ]; then
-    result=$(($1 / $2))
-  else
-    result="Error: Division by zero is not allowed."
-  fi
-}
-
-# Function to perform modulo
-function modulo() {
-  if [ $2 -ne 0 ]; then
-    result=$(($1 % $2))
-  else
-    result="Error: Division by zero is not allowed."
-  fi
-}
-
-# the Main script starts here
+# Main script starts here
 
 echo "Please enter six numbers separated by spaces:"
-# read $num1 $num2 $num3 $num4 $num5 $num6
+read num1 num2 num3 num4 num5 num6
 
-addition $num1 $num2
-echo "Addition of $num1 and $num2 is: $result"
-
-subtraction $num3 $num4
-echo "Subtraction of $num3 and $num4 is: $result"
-
-multiplication $num5 $num6
-echo "Multiplication of $num5 and $num6 is: $result"
-
-division $num1 $num3
-echo "Division of $num1 by $num3 is: $result"
-
-modulo $num2 $num4
-echo "Modulo of $num2 by $num4 is: $result"
+addition $num1 $num2 $num3 $num4 $num5 $num6
+echo "The sum of $num1, $num2, $num3, $num4, $num5, and $num6 is: $result"
