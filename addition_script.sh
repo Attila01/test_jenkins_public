a11e1ec7fac977fdeee5629d07e5680000f90901
Attila01 <48849467+Attila01@users.noreply.github.com>
2023-07-27 13:56:53 -0400

    added a 3

--- addition_script.sh
+++ addition_script.sh
@@ -9,7 +9,7 @@ function addition() {
 
 echo "Please enter two numbers separated by a space:"
 # read num1 num2
-# read num1 num2
+# read num1 num23
 
 addition $num1 $num2
 echo "The sum of $num1 and $num2 is: $result"
